732e77fdf68ea7617645da32bd72969ba9d4bfcc
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2024-11-11 06:20:05 +0100

    message

--- javascript/class1/script.js
+++ javascript/class1/script.js
@@ -817,16 +817,597 @@
 
 // x = -b + or - sqrt(b^2 - 4ac) / 2a
 
-function quad(a, b, c) {
-  let d = b ** 2 - 4 * a * c;
-  let sqrtd = d ** 0.5;
-  let solOneNumerator = -b + sqrtd;
-  let solTwoNumerator = -b - sqrtd;
-  let sol1 = solOneNumerator / (2 * a);
-  let sol2 = solTwoNumerator / (2 * a);
-  console.log(sol1 + "," + sol2);
-}
+// function quad(a, b, c) {
+//   let d = b ** 2 - 4 * a * c;
+//   let sqrtd = d ** 0.5;
+//   let solOneNumerator = -b + sqrtd;
+//   let solTwoNumerator = -b - sqrtd;
+//   let sol1 = solOneNumerator / (2 * a);
+//   let sol2 = solTwoNumerator / (2 * a);
+//   console.log(sol1 + "," + sol2);
+// }
 
-// solve x^2 - 3x + 2;
+// // solve x^2 - 3x + 2;
 
-quad(1, -3, 2);
+// quad(1, -3, 2);
+
+// function greet(name = "gb", time = "morning") {
+//   // let time = "morning";
+//   return "Hello " + name + " good " + time;
+// }
+
+// console.log(greet("James"));
+
+// // there are two ways
+// 1) square bracket
+// 2) array contructor function/method
+
+// let myArr = ["Nk", "Gb"];
+
+// // myArr.push("James");
+// // myArr.push("Daniel");
+
+// console.log(myArr);
+
+// var myArr = new Array("gb", "fm", "lc");
+// myArr.push("Nk", "Omj");
+// myArr.push("James");
+// var myArr = [];
+// console.log(myArr);
+
+// informations coming from a db or api are usually in the form of arrays
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+// basic array methods
+// push=> adds to the end of an array
+
+// let myFruits = [];
+
+// myFruits.push("mango", "apple", "banana");
+
+// console.log(myFruits);
+
+// pop=> removes from the end of an array
+
+// myFruits.pop();
+// myFruits.pop();
+
+// console.log(myFruits);
+
+// unshift=> adds to the begining of an array
+
+// myFruits.unshift("pear", "watermelon");
+// console.log(myFruits);
+
+// shift=> removes from the begining of an array
+// myFruits.shift();
+// myFruits.shift();
+// console.log(myFruits);
+
+// let myFruits = ["apple", "banana", "cashew", "watermelon", "pear", "grape"];
+
+// // console.log(myFruits);
+
+// let slicedArr = myFruits.slice(1, 3); //(first and lastIndex-1)
+
+// console.log(slicedArr);
+// // console.log(myFruits);
+
+// let splicedArr = myFruits.splice(1, 3); //(first and lastlength-1)
+
+// console.log(splicedArr);
+
+// // console.log(myFruits);
+
+// myFruits.splice(2, 3);
+// // splice can remove or add to any position in an array
+// // 1) when its removing it takes two arugments
+// // 2) when its adding it takes three arguments
+
+// console.log(myFruits);
+
+// question:
+// 1) when you load the browser, it should show an alert (welcome, do you wish to buy from us today? (y/n))
+// 2) when the user says no (it should thanks for visiting)
+// 3) when the user says yes (it should take the user into the applicaion)
+// 4) it shoud ask do you wish to add or display or quit (y/n)
+// 5) if the user's reply is add
+// 5) it should say enter a fruit you would like to add
+// 6) if the user's reply is display
+// 7) it should display all the fruits
+// 8) if the user's reply is quit
+// 9) it should say thanks for shopping with us today
+
+// looping through an array
+
+// for loop and while loop to loop through an array
+
+// myFruits.forEach((fruit) => {
+//   console.log(fruit);
+// });
+
+// myFruits.forEach(function (fruit) {
+//   console.log(fruit);
+// });
+
+// myFruits.map((fruit) => {
+//   console.log(fruit);
+// });
+
+// myFruits.map(function (fruit) {
+//   console.log(fruit);
+// });
+
+// filtering an array using the for loop
+// let result = [];
+// for (let i = 0; i < myFruits.length; i++) {
+//   if (myFruits[i] !== "banana") {
+//     result.push(myFruits[i]);
+//   }
+// }
+
+// console.log(result);
+
+// filter method=> removes an element from an array
+// let result = myFruits.filter((fruit) => fruit !== "banana");
+
+// console.log(result);
+
+// // reduce method=> adds all the elements of an array
+// let scores = [60, 57, 99, 67];
+
+// let total = scores.reduce((a, b) => a + b);
+// let total = scores.reduce((a, b) => {
+//   return a + b;
+// });
+// console.log(total);
+
+// adding the elements of an array using for loop
+
+// let acc = 0;
+
+// for (let i = 0; i < scores.length; i++) {
+//   // acc = acc + scores[i];
+//   acc += scores[i];
+// }
+
+// console.log(acc);
+
+// sort => arrange an array in ascending or descending order
+
+// let asc = scores.sort((a, b) => a - b);
+// console.log(asc);
+// let des = scores.sort((a, b) => b - a);
+// console.log(des);
+
+// let myFruits = ["apple", "cashew", "Wallnut"];
+
+// let sorted = scores.sort();
+// console.log(sorted);
+
+// let sorted = myFruits.sort();
+// console.log(sorted);
+
+// function functionName() {}
+
+// let funct = () => {};
+
+// let myScore = scores.find((x) => x === 99);
+// let myScore = scores.findIndex((x) => x === 99);
+// console.log(myScore);
+
+// let multiDim = [
+//   [1, 2],
+//   [3, 4],
+//   [5, 6],
+// ];
+
+// console.log(multiDim);
+
+// // flat method=> reduces the dimension of an array
+// let reducedDim = multiDim.flat();
+
+// console.log(reducedDim);
+
+// const myGoods = [
+//   "mango",
+//   "apple",
+//   "banana",
+//   "orange",
+//   "sha type any fruit brother",
+// ];
+
+// const message = prompt(
+//   "Welcome to my store my bro, Do you wish to buy from us today? (yes/no)"
+// );
+
+// if (message === "yes") {
+//   const action = prompt(
+//     "Do you wish to add, display, or quit? (add/display/quit)"
+//   );
+
+//   if (action === "add") {
+//     const fruit = prompt("Enter a fruit you would like to add:");
+//     myGoods.push(fruit);
+//     alert(fruit + " has been added to your cart.");
+//   } else if (action === "display") {
+//     alert("Here are all the fruits we have: " + myGoods.join(", "));
+//   } else if (action === "quit") {
+//     alert("Thanks for shopping with us today!");
+//   } else {
+//     alert("Invalid.");
+//   }
+// } else if (message === "no") {
+//   alert("Thanks for visiting!");
+// } else {
+//   alert("Invalid. Please reload the page and try again.");
+// }
+
+// let welcome = prompt("Welcome to AB mall. Do you want to shop with us? (Y/N)");
+// welcome = welcome.toUpperCase();
+// const shopping = ["Mango", "Banana"];
+// let items;
+// let moreFruit;
+
+// if (welcome === "Y") {
+//   let option;
+
+//   do {
+//     option = prompt("Do you want to add, display or exit? (a, d, q)");
+
+//     if (option === "a") {
+//       items = prompt("Enter a fruit you would like to add:");
+//       shopping.push(items);
+
+//       moreFruit = prompt(
+//         "Do you want to add another fruit? (Y/N)"
+//       ).toUpperCase();
+//       while (moreFruit === "Y") {
+//         items = prompt("Enter a fruit you would like to add:");
+//         shopping.push(items);
+//         moreFruit = prompt(
+//           "Do you want to add another fruit? (Y/N)"
+//         ).toUpperCase();
+//       }
+//     } else if (option === "d") {
+//       alert("Your shopping list: " + shopping.join(", "));
+//     } else if (option === "q") {
+//       alert("Thanks for shopping with us today!");
+//     } else {
+//       alert("Invalid option, please choose a, d, or q.");
+//     }
+//   } while (option !== "q");
+// } else if (welcome === "N") {
+//   alert("Thanks for visiting!");
+// } else {
+//   alert("Invalid input. Please enter Y or N.");
+// }
+
+// Array to store the list of fruits
+// let fruits = [];
+
+// // Function to display the fruits
+// function displayFruits() {
+//   if (fruits.length > 0) {
+//     alert(fruits.join(", "));
+//   } else {
+//     alert("No fruits in the list.");
+//   }
+// }
+
+// // Welcome the user and ask if they wish to buy
+// function welcomeUser() {
+//   let response = prompt("Welcome, do you wish to buy from us today? (yes/no)");
+//   if (response === "yes") {
+//     enterApplication();
+//   } else {
+//     alert("Thanks for visiting!");
+//     return;
+//   }
+// }
+
+// // Main application logic
+// function enterApplication() {
+//   while (true) {
+//     let action = prompt(
+//       "Do you wish to add, display, or quit? (type 'add', 'display', or 'quit')"
+//     );
+
+//     if (action === "add") {
+//       let fruit = prompt("Enter a fruit you would like to add:");
+//       if (fruit) {
+//         fruits.push(fruit);
+//         alert(`${fruit} has been added to your list.`);
+//       } else {
+//         alert("No fruit entered.");
+//       }
+//     } else if (action === "display") {
+//       displayFruits();
+//     } else if (action === "quit") {
+//       alert("Thanks for shopping with us today!");
+//       break;
+//     } else {
+//       alert("Invalid option. Please type 'add', 'display', or 'quit'.");
+//     }
+//   }
+// }
+
+// // Start the script by welcoming the user
+// welcomeUser();
+
+// object
+// let person = {
+//   firstName: "Godbless",
+//   lastName: "Ejovwo",
+//   email: "ejovwogfreeman007@gmail.com",
+//   age: 55,
+//   isAdmin: true,
+//   isMarried: false,
+//   kids: null,
+//   school: "pluralcode_academy",
+//   courses: ["html", "css", "js"],
+//   address: {
+//     facebook: "ejovwogfreeman",
+//     twitter: "ejovwogfreeman",
+//     instagram: "ejovwogfreeman",
+//   },
+// };
+
+// person.level = "advanced";
+// person["remark"] = "excellent";
+// person.isMarried = true;
+
+// console.log(person);
+
+// for(let i in person){
+//   console.log(i, person[i]);
+// }
+
+// console.log(person.firstName);
+
+// destructuring an object
+// const { firstName, lastName } = person;
+
+// console.log(lastName);
+
+// const person = {
+//   name: "Godbless",
+//   email: "ej@gmail.com",
+//   age: 55,
+// };
+
+// console.log(person);
+
+// function Person(name, email, age) {
+//   this.name = name;
+//   this.email = email;
+//   this.age = age;
+//   this.greet = function () {
+//     return `Hello, my name is ${this.name}`;
+//   };
+// }
+
+// const person1 = new Person("godbless", "ej@gmail.com", 44);
+// const person2 = new Person("freeman", "fm@gmail.com", 25);
+
+// console.log(person2.greet());
+
+// as set
+
+// const mySet = new Set([1, 2, 3, 3, 3, 4, 5, 5]);
+
+// console.log(mySet);
+
+// set methods
+// 1)add 2)delete 3) has 4) size 5) clear
+
+// mySet.add(55);
+// mySet.delete(3);
+// console.log(mySet.has(509));
+// console.log(mySet.size);
+// mySet.clear();
+// console.log(mySet.size);
+// console.log(mySet);
+
+// console.log(mySet);
+
+// map
+
+// const map = new Map([["key", "value"]]);
+
+// const myMap = new Map([
+//   ["firstName", "Godbless"],
+//   ["lastName", "Ejovwo"],
+// ]);
+
+// console.log(myMap);
+
+// myMap.set("email", "ej@gmail.com");
+
+// console.log(myMap.get("firstName"));
+
+// console.log(myMap.size);
+
+// const myMap = new Map([
+//   ["firstName", "Godbless"],
+//   ["lastName", "Ejovwo"],
+//   ["email", "ej@gmail.com"],
+//   ["phone", "080777"],
+//   ["gender", "Male"],
+//   ["age", 55],
+// ]);
+
+// const myObj = {
+//   firstName: "Godbless",
+//   lastName: "Ejovwo",
+//   email: "ej@gmail.com",
+//   phone: "080777",
+//   gender: "Male",
+//   age: 55,
+// };
+
+// console.log(myMap.size);
+// console.log(Object.keys(myObj).length);
+
+// let myVar = [];
+// console.log(Array.isArray(myVar));
+
+// let myStr = "Pluralcode_Ademy";
+
+// str methods
+// length, charAt(), indexOf(), slice(), substr(), substring(), toUpperCase(), toLowerCase(), trim(), split(), replace().
+
+// console.log(myStr.length);
+
+// indexing a str
+
+// // first char of every str
+// console.log(myStr[0]);
+// // last char of every str
+// console.log(myStr[myStr.length - 1]);
+
+// console.log(myStr.charAt(myStr.length - 1));
+
+// console.log(myStr.indexOf("a"));
+// console.log(myStr.lastIndexOf("a"));
+
+// console.log(myStr.slice(6, 10));
+// console.log(myStr.substring(6, 10));
+
+// console.log(myStr.substr(6, 4));
+
+// console.log(myStr.toUpperCase());
+// console.log(myStr.toLowerCase());
+
+// let myStr = " godbless ";
+// let trimmedString = myStr.trim();
+
+// console.log(myStr.length);
+// console.log(trimmedString.length);
+
+// split - converts a string to an array
+
+// let myStr = "godbless";
+
+// let myStr = "PluralCode_Academy";
+
+// console.log(myStr.split("_"));
+
+// let myStr = "PluralCode_Academy";
+
+// let newStr = myStr.replace("PluralCode", "gbmedia");
+
+// console.log(newStr);
+
+// let str = "godbless";
+
+// console.log(str.replace("god", "jesus"));
+
+// function createUsernameFromEmail(email) {
+//   let username = email.split("@")[0];
+//   return username;
+// }
+
+// const username = createUsernameFromEmail("pluralcode.company@pluralcode.com");
+
+// console.log(username);
+
+// regular expression
+
+// function createUsernameFromEmail(email) {
+//   let username = email.split("@")[0];
+//   return username;
+// }
+
+// const username = createUsernameFromEmail("pluralcode.company@pluralcode.com");
+
+// console.log(username);
+
+// /pattern/modifiers;
+
+// // let text = "Visit Microsoft";
+// let text =
+//   "I love Microsoft because Microsoft is a great place to work and Microsoft has the best Engineers";
+
+// // let result = text.replace(/microsoft/i, "Pluralcode");
+// // let result = text.replace(/microsoft/gi, "Pluralcode");
+
+// console.log(result);
+
+// anchors
+// /^a/ => match all a at the begining of a
+// /a$/ => match all a at the end of a string
+
+// character class
+// \d => match digits (0 - 9)
+// \w => any word (letter, underscore, digits)
+// \s => match white space character
+// . => match any character apart from a new line
+
+// quantifiers
+// * zero or more
+// + one or more
+// {n} exact n times
+
+// let emailRegex = /^[W.-]+@[/
+
+// let number = 834723984738.5748937;
+
+// console.log(number.toString().replace(/B(?=(d{3})+(?!d))/g, ","));
+
+// const str = new String("hell world");
+
+// console.log(str);
+
+// let date = new Date();
+
+// let day = new Date().getDay();
+
+// let month = new Date().getMonth();
+
+// new Date().toDateString();
+
+// let dayOfWeek = [
+//   "sunday",
+//   "monday",
+//   "tuesday",
+//   "wednesday",
+//   "thursday",
+//   "friday",
+//   "saturday",
+// ];
+
+// console.log(month);
+// console.log(new Date().toDateString());
+// console.log(new Date().toLocaleDateString());
+// console.log(new Date().toLocaleTimeString());
+
+// console.log(new Date().getTime());
+
+// console.log(new Date().getDate());
+
+// console.log(
+//   "posted on " +
+//     new Date().toDateString() +
+//     " | " +
+//     new Date().toLocaleTimeString()
+// );
+
+// js math object
+
+// console.log(Math.PI);
+
+// console.log(Math.pow(2, 3));
+
+// let num = 15.5;
+
+// console.log(Math.round(num)); // round off number (either up or down)
+// console.log(Math.floor(num)); // always round down
+// console.log(Math.ceil(num)); // always round up
+
+// console.log(Math.max(3, 9, 78));
+// console.log(Math.min(3, 9, 78));
+
+console.log(Math.random());
